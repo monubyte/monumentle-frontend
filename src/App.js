e88b4b2a03a9468ac9a0b155380ddf22ad4c312d
cloudui <ericchen314@gmail.com>
2022-04-10 02:25:59 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [img, setIMG] = useState(null);
+  
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/2').then((response) => {
+      setIMG(response.data);
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error);
+      }
+    })
+  })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={img.label} className="image" alt="missing"/>
     </div>
   );
 }
